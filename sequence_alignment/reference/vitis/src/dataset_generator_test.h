36e3b8c2acb9b9da8379e5ded951fd993f321175
vandinhtranengg <135353645+vandinhtranengg@users.noreply.github.com>
2026-07-27 12:14:12 +0900

    Delete sequence_alignment directory

--- sequence_alignment/reference/vitis/src/dataset_generator_test.h
+++
@@ -1,6 +0,0 @@
-#ifndef DATASET_GENERATOR_TEST_H
-#define DATASET_GENERATOR_TEST_H
-
-int run_dataset_generator_tests();
-
-#endif
--- sequence_alignment/reference/vitis/src/software_regression.h
+++
@@ -1,6 +0,0 @@
-#ifndef SOFTWARE_REGRESSION_H
-#define SOFTWARE_REGRESSION_H
-
-int run_software_alignment_regression();
-
-#endif
